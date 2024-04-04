b222d805cf53be3d3209efdaa734c623bb1d6256
Divya-Manivannan1 <reethudya@gmail.com>
2024-04-04 19:29:48 +0100

    added come back later page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Intro } from "./pages/Intro/Intro";
 import "./App.scss";
 import Home from "./pages/Home";
+import { ComeBackLater } from "./pages/ComeBackLater/ComeBackLater";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <BrowserRouter basename="">
         <Routes>
           <Route path="/" element={<Intro />} />
+          <Route path="/ComeBackLater" element={<ComeBackLater />} />
           <Route path="/home" element={<Home />} />
           <Route path="/beer/:ID" element={<></>} />
         </Routes>
